ec066f7cdc17fef164f56cfe17ec1b716ccf3425
Gabriel Assis <gabrielassisdepaula@gmail.com>
2026-04-21 22:51:29 -0300

    Fix visual design inconsistencies from design review
    - transaction_card.html: rounded-lg -> rounded-box for DaisyUI consistency
    - upload_result.html: rounded -> rounded-box, add border for visual separation
    - chat_message.html: parse attachments JSON instead of showing raw string
    - templates/__init__.py: add fromjson Jinja2 filter

--- src/templates/__init__.py
+++ src/templates/__init__.py
@@ -1,5 +1,7 @@
+import json
 from jinja2 import Environment, FileSystemLoader
 from starlette.templating import Jinja2Templates
 
 env = Environment(loader=FileSystemLoader("src/templates"), autoescape=True, cache_size=0)
+env.filters["fromjson"] = lambda s: json.loads(s)
 templates = Jinja2Templates(env=env)
